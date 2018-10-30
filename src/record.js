f25ebb4f8710b123d5f77a427ad59bc15227f334
frankelavsky <frankelavsky@gmail.com>
2018-10-29 22:44:37 -0500

    fixed styling in record component

--- src/record.js
+++ src/record.js
@@ -1,31 +1,43 @@
 import React from "react";
-import List from "./list";
-import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import Details from "./details";
+import { Draggable } from "react-beautiful-dnd";
+
+const grid = 4;
+const getItemStyle = (isDragging, draggableStyle) => ({
+  userSelect: "none",
+  padding: 0,
+  margin: `0 0 ${grid}px 0`,
+  height: 250 - grid * 2,
+  background: isDragging ? "rgba(53, 53, 53, 0.75)" : "",
+
+  ...draggableStyle
+});
 
 const Record = props => {
   return (
-    <div>
-      <div>
-        <h1>{props.title}</h1>
-        <div className="year">
-          <div>Released:</div>
-          <div>{props.year}</div>
+    <Draggable draggableId={props.draggableId} index={props.index}>
+      {(provided, snapshot) => (
+        <div
+          ref={provided.innerRef}
+          {...provided.draggableProps}
+          {...provided.dragHandleProps}
+          style={getItemStyle(
+            snapshot.isDragging,
+            provided.draggableProps.style
+          )}
+          className="record"
+        >
+          <div>
+            <div>
+              <h1 className="recordTitle">
+                {props.release.basic_information.title.toUpperCase()}
+              </h1>
+              <Details info={props.release} isDragging={snapshot.isDragging} />
+            </div>
+          </div>
         </div>
-      </div>
-
-      <div className="column3">
-        <h2>By:</h2>
-        {/* List Module */}
-      </div>
-      <div className="column3">
-        <h2>Labels:</h2>
-        {/* List Module */}
-      </div>
-      <div className="column3">
-        <h2>Formats:</h2>
-        {/* List Module */}
-      </div>
-    </div>
+      )}
+    </Draggable>
   );
 };
 
