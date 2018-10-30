21f935a745b7c93847e1c0e9bd4f479cd10030ab
frankelavsky <frankelavsky@gmail.com>
2018-10-30 00:54:48 -0500

    finished add button styling and rendering

--- src/addButton.js
+++ src/addButton.js
@@ -1,9 +1,15 @@
 import React from "react";
 
 const AddButton = props => {
+  let buttonStyle = {};
+  buttonStyle.top = props.top - 8 + "px";
   return (
-    <button className="addButton" onClick={e => props.addFunction(e)}>
-      <div className="buttonX">x</div>
+    <button
+      className="addButton"
+      style={buttonStyle}
+      onClick={e => props.addFunction(e)}
+    >
+      <div className="buttonPlus">+</div>
     </button>
   );
 };
